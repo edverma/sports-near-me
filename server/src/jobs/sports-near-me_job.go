d17e1dcbed280ae7dd6059ae8396b8845d708c14
Ilana Bonell <ilana.bonell@gmail.com>
2023-09-09 13:31:52 -0400

    created different types to pull specific data from minor league API. created a new function to unmarshal the JSON from API. Printed the JSON data we wanted from our types to the sports-near-me logger.

--- server/src/jobs/sports-near-me_job.go
+++ server/src/jobs/sports-near-me_job.go
@@ -2,8 +2,10 @@ package jobs
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"sync"
 	"time"
@@ -29,17 +31,74 @@ func RunSportsNearMeJob(parentCtx context.Context, once *sync.Once) {
 	s.StartBlocking()
 }
 
+type HomeTeamName struct {
+	Name string `json:"name"`
+}
+
+type AwayTeamName struct {
+	Name string `json:"name"`
+}
+
+type Location struct {
+	Address1   string `json:"address1"`
+	City       string `json:"city"`
+	Country    string `json:"USA"`
+	PostalCode string `json:"postalCode"`
+	State      string `json:"state"`
+}
+
+type Venue struct {
+	Location Location `json:"location"`
+	Name     string   `json:"name"`
+}
+
+type Away struct {
+	AwayTeamName AwayTeamName `json:"team"`
+}
+
+type Home struct {
+	HomeTeamName HomeTeamName `json:"team"`
+}
+
+type Team struct {
+	Away Away `json:"away"`
+	Home Home `json:"home"`
+}
+
+type Games struct {
+	Teams Team  `json:"teams"`
+	Venue Venue `json:"venue"`
+}
+
+type Date struct {
+	Date       string  `json:"date"`
+	TotalGames int     `json:"totalGames"`
+	Games      []Games `json:"games"`
+}
+
+type ScheduleResponse struct {
+	Dates []Date `json:"dates"`
+}
+
 func (j *job) sportsNearMeJob(cron gocron.Job) {
 	j.l.Printf("running sports-near-me job....")
 
-	resp, err := http.Get("https://www.milb.com/gwinnett/schedule/2023-09")
+	resp, err := http.Get("https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=11,12,13,14,15,16,5442&hydrate=team(venue(timezone,location)),venue(timezone,location),game(seriesStatus,seriesSummary,tickets,promotions,sponsorships,content(summary,media(epg))),seriesStatus,seriesSummary,decisions,person,linescore,broadcasts(all)&season=2023&startDate=2023-07-01&endDate=2023-07-31&teamId=431&eventTypes=primary&scheduleTypes=games,events,xref")
 	if err != nil {
 		j.l.Printf("failed to get HTTP. error: %v", err)
 	}
+
+	//defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		j.l.Printf("failed to read all HTTP. error: %v", err)
 	}
-	j.l.Printf(string(body))
+
+	var res ScheduleResponse
+	jsonErr := json.Unmarshal(body, &res)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	j.l.Printf("%v", res)
 }
