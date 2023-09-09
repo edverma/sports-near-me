7983eae6c3235ccf40fc88321f77167b98b6a6cb
Ilana Bonell <ilana.bonell@gmail.com>
2023-09-09 13:35:44 -0400

    remove comments

--- server/src/cache/init.go
+++ server/src/cache/init.go
@@ -34,8 +34,6 @@ func Initialize() *Client {
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
-	//connection string: client:= ConnectionMultiplexer.Connect($"{Host_name}:{Port_Number},password={pass}");
-	// Need to grab db connection and  run = redis.GetDatabase().Ping()
 	seedData(client)
 
 	return &Client{l, client}
